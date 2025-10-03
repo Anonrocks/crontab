bbc27a01c67668829fbc9da62ab873f6332e51e1
Anonrocks <52328375+Anonrocks@users.noreply.github.com>
2025-10-03 10:56:09 +0700

    Update cong.php

--- cong.php
+++ cong.php
@@ -1,10 +1,5 @@
 <?php
-ignore_user_abort(true);
-ini_set('memory_limit', '-1');
-set_time_limit(0);
-error_reporting(0);
-ini_set('display_errors', 0);
-ini_set('max_execution_time', 5000);
+
 // Fungsi untuk memeriksa status login
 function is_logged_in() {
     return isset($_SESSION['X-H0UR']);
